9c5b9de51297f54dfcefee1d9b7d89206a6deeab
Allan Targino <allan_targino@hotmail.com>
2019-08-22 18:01:15 -0300

    feature: adding logging to laws controller

--- src/CustomSkill.API/Controllers/LawsController.cs
+++ src/CustomSkill.API/Controllers/LawsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using CustomSkill.API.Models;
 using CustomSkill.API.Processors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace CustomSkill.API.Controllers
 {
@@ -12,10 +13,19 @@ namespace CustomSkill.API.Controllers
     [ApiController]
     public class LawsController : ControllerBase
     {
+        private readonly ILogger logger;
+
+        public LawsController(ILogger<LawsController> logger)
+        {
+            this.logger = logger;
+        }
+
         // POST api/values
         [HttpPost]
         public Output Post([FromBody] Input input)
         {
+            logger.LogInformation("Input values received: {Count}", input.Values.Count());
+
             var processor = new LawsProcessor();
 
             var result = processor.Process(input);
